95201db1d9cc3010fd891c36f1c0d8a9d6088aeb
shrestha <shresthaswai99@gmail.com>
2021-04-16 21:35:45 +0530

    first commit

---
+++ src/test/java/Maven/Mavenjava/AppiumTest.java
@@ -0,0 +1,17 @@
+package Maven.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void Android()
+	{
+System.out.println("Android");
+	
+	}
+@Test
+public void IOS()
+{
+System.out.println("IOS");
+}}
---
+++ src/test/java/Maven/Mavenjava/SeleniumTest.java
@@ -0,0 +1,21 @@
+package Maven.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserAutomation()
+	{
+System.out.println("BrowserAutomation");
+	}
+
+	@Test
+	public void Elements() {
+
+System.out.println("BrowserAutomation");
+
+}}
+
+// In this testng.xml is for regression testing as it has all the test cases whereas testng2.xml is smoke testing where we are testing only the required
+ //or main test cases.
